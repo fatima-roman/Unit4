14def892d1c90550570c363534fb3d2feb5d636e
fatima-roman <fatima.roman@iesnervion.es>
2026-01-12 10:19:38 +0100

    construcor

--- src/boletin1/EJ01.java
+++ src/boletin1/EJ01.java
@@ -34,6 +34,30 @@ public class EJ01 {
         System.out.println("Punto 1: (" + p1.x + ", " + p1.y + ")");
         System.out.println("Punto 2: (" + p2.x + ", " + p2.y + ")");
         System.out.println("Punto 3: (" + p3.x + ", " + p3.y + ")");
+        
+        
+        Punto p4 = new Punto(4,9);
+        Punto p5 = new Punto(5,2);
+        Punto p6 = new Punto(7,10);
+        
+        System.out.println("Punto 1: (" + p4.x + ", " + p4.y + ")");
+        System.out.println("Punto 2: (" + p5.x + ", " + p5.y + ")");
+        System.out.println("Punto 3: (" + p6.x + ", " + p6.y + ")");
+        
+        p4.x += 3;
+        p4.y -= 2;
+
+        p5.x *= 2;
+        p5.y += 5;
+
+        p6.x = p3.x + 1;
+        p6.y /= 2;
+        
+        System.out.println("Después de modificar las coordenadas:");
+        System.out.println("Punto 1: (" + p4.x + ", " + p4.y + ")");
+        System.out.println("Punto 2: (" + p5.x + ", " + p5.y + ")");
+        System.out.println("Punto 3: (" + p6.x + ", " + p6.y + ")");
+        
 	}
 
 }
--- src/boletin1/Punto.java
+++ src/boletin1/Punto.java
@@ -3,5 +3,13 @@ package boletin1;
 class Punto {
     int x;
     int y;
-}
 
+	public Punto (int x, int y) {
+		this.x = x;
+		this.y = y; 
+	}
+	
+	public Punto() {
+		
+	}
+}
